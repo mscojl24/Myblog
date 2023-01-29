a0f117c0f4fbbf54acc95341a411d26ac1226203
mscojl24 <mscojl24@naver.com>
2023-01-29 20:53:47 +0900

    icon 셀렉트버튼 기능추가

--- src/App.js
+++ src/App.js
@@ -87,24 +87,36 @@ function Memolist() {
   const [posts, setPost] = useState(dummyPost)
   const [username, setUsername] = useState("")
   const [memo, setMemo ] = useState("")
+  const [icon, setIcon ] = useState("https://i.imgur.com/uqyWEHw.png")
 
-  const handleChangeName = (e) => {
+  const icons = ["https://i.imgur.com/uqyWEHw.png","https://i.imgur.com/GBIyZf5.png","https://i.imgur.com/uuZlAF9.png","https://i.imgur.com/z5J7otb.png"];
+  const iconname = ["icon_01","icon_02","icon_03","icon_04"];
+
+  const options = icons.map((icons,i) => {
+      return <option value={icons}>{iconname[i]}</option>
+  });
+
+  const handleIcon = (e) => { //아이콘 선택박스
+    setIcon(e.target.value)
+  };
+
+  const handleChangeName = (e) => { //유저이름 출력
     setUsername(e.target.value)
   };
 
-  const handleChangeMemo = (e) => {
+  const handleChangeMemo = (e) => { //유저메모 출력
     setMemo(e.target.value)
   };
 
-  const handleClickBtn = (e) => {
+  const handleClickBtn = (e) => { // 클릭 시 포스팅
     const post = {
       id: dummyPost.length,
       username: username,
-      picture: "https://i.imgur.com/GBIyZf5.png",
+      picture: icon,
       content : memo,
       createdAt : new Date().toLocaleDateString('ko-kr'),
     };
-    setPost([posts, ...posts])
+    setPost([post, ...posts])
   };
   
 
@@ -116,13 +128,7 @@ function Memolist() {
         <span className='notice__text'>이것은 공지사항입니다. 공지사항테스트.</span>
       </div>
       <div className='posting__box memo__notice'>
-        <select name="icon">
-            <option value="">Select icon</option>
-            <option value="https://i.imgur.com/GBIyZf5.png">icon_01</option>
-            <option value="https://i.imgur.com/uqyWEHw.png">icon_02</option>
-            <option value="https://i.imgur.com/5txsjOR.png">icon_03</option>
-            <option value="https://i.imgur.com/z5J7otb.png">icon_04</option>
-        </select>
+        <select onChange={handleIcon}>{options}</select>
         <input className='post_input' placeholder='닉네임' value={username} onChange={handleChangeName}/>
         <input className='post_input' placeholder='오늘의 이야기를 들려주세요' value={memo} onChange={handleChangeMemo}/>
         <button className='post_btn' onClick={handleClickBtn}> Submit </button>
